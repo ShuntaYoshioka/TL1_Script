f8c01810135c2b9d4d72ff1a49bbdd64c0f7dd2f
ShuntaYoshioka <K024G0096@g.neec.ac.jp>
2026-06-09 11:57:08 +0900

    Update level_editor.py
    01-04 完成

--- level_editor.py
+++ level_editor.py
@@ -14,11 +14,42 @@ bl_info = {
     "category": "Object",
 }
 
+def draw_menu_manual(self, context):
+    self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    bl_label = "MyMenu"
+    bl_description = "拡張メニュー by " + bl_info["author"]
+
+    def draw(self, context):
+
+        self.layout.operator("wm.url_open_preset", 
+                             text="Manual", icon='HELP')
+
+    def submenu(self, context):
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+
+# 登録するクラスのリスト
+classes = (
+    TOPBAR_MT_my_menu,
+)
+
 def register():
-    print("アドオンが有効化されました.")
+    # クラスの登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+    print("レベルエディタ（アドオン）が有効化されました.")
 
 def unregister():
-    print("アドオンが無効化されました.")
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
+    print("レベルエディタ（アドオン）が無効化されました.")
     
 # テスト実行用コード
 if __name__ == "__main__":
