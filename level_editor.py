6ac8ca6f3ff46d5a30ccc2e7de18051f8fb127b9
ShuntaYoshioka <K024G0096@g.neec.ac.jp>
2026-06-16 03:57:55 +0900

    Update level_editor.py
    01-06 多分

--- level_editor.py
+++ level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 # Blenderに登録するアドオンの情報
 bl_info = {
@@ -46,6 +47,44 @@ class MYAFFON_OT_create_ico_sphere(bpy.types.Operator):
         print("ICO球を生成しました")
 
         return {'FINISHED'}
+    
+
+class  MYAFFON_OT_export_scene(bpy.types.Operator):
+     bl_idname = "myaddon.myaddon_ot_export_scene"
+     bl_label = "scene出力"
+     bl_description ="scene情報をExportします"
+
+     def execute(self, context):
+        print(" scene情報をExportします")
+
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)
+
+            trans, rot, scale = object.matrix_local.decompose()
+
+            rot = rot.to_euler()
+
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+
+            print("Trans(%f,%f,%f)" % (trans.x,trans.y,trans.z))
+            print("Rot(%f, %f, %f)" % (rot.x,rot.y,rot.z))
+            print("Scale(%f, %f, %f)" % (scale.x, scale.y, scale.z))
+
+            if object.parent:
+                print("Parent:" + object.parent.name)
+            print()
+
+
+        print(bpy.context.scene.objects)
+
+        print("scene情報をExportしました")
+        self.report({'INFO'}, "scene情報をExportしました")
+
+        return {'FINISHED'}
+
+
 
     
 
@@ -62,9 +101,12 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname,
              text=MYADDON_OT_stretch_vertex.bl_label)
         
-        # ⭕ draw 関数の中に追記
+        #draw 関数の中に追記
         self.layout.operator(MYAFFON_OT_create_ico_sphere.bl_idname, 
                      text=MYAFFON_OT_create_ico_sphere.bl_label)
+        
+        self.layout.operator(MYAFFON_OT_export_scene.bl_idname,
+                     text=MYAFFON_OT_export_scene.bl_label)
 
     def submenu(self, context):
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
@@ -74,6 +116,7 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
 classes = (
     MYADDON_OT_stretch_vertex,
     MYAFFON_OT_create_ico_sphere,
+    MYAFFON_OT_export_scene,
     TOPBAR_MT_my_menu,
 )
 
